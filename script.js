7e270f653d50f92f94caf69d3bd5c77c514247f3
Victoria Ferry <ferryvictoria@hotmail.fr>
2025-07-04 12:08:00 +0200

    ajout de style

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 const btnSubmit = document.getElementById('btn-submit')
 const divQuoteList = document.getElementById('quote-list')
- 
+const inputQuote = document.getElementById('quote-entry')
+const inputAuthor = document.getElementById('author-name')
+
+let quoteCount = 1
+let citationStorage = localStorage
 
 btnSubmit.addEventListener('click', addQuote)
 
 // Ajout de la citation et de l'auteur 
-function addQuote(quote, author){
+function addQuote(){
     const quoteP = document.createElement('p')
     quoteP.className = 'text'
     quoteP.innerText = document.getElementById('quote-entry').value 
-    console.log(quote)
 
     const authorP = document.createElement('p')
-    authorP.className = 'text'
+    authorP.className = 'author'
     authorP.innerText = document.getElementById('author-name').value 
-    console.log(author)
 
     const divQuote = document.createElement('div')
     divQuote.className = 'quote'
@@ -22,4 +24,11 @@ function addQuote(quote, author){
     divQuote.appendChild(quoteP)
     divQuote.appendChild(authorP)
     divQuoteList.appendChild(divQuote)
+
+    quoteCount += 1
+    const count = document.getElementById('count')
+    count.innerText = `${quoteCount} citation(s)`
+
+    inputQuote.value = ''
+    inputAuthor.value = ''
 }
