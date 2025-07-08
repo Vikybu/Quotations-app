cdebfd19b50d9546a7f1404a33dd70a0ea244668
Victoria Ferry <ferryvictoria@hotmail.fr>
2025-07-08 10:52:54 +0200

    LocalStorage fonctionnel et compteur de citation fonctionnel

--- script.js
+++ script.js
@@ -5,102 +5,66 @@ const inputAuthor = document.getElementById('author-name')
 
 let quoteCount = 1
 let totalCount = 0
-let newStorage = {}
+
+if(localStorage.getItem('quotes') == null){
+    localStorage.setItem('quotes', '')
+}
+
+addQuote()
+quoteTotalCount()
+
+btnSubmit.addEventListener('click',
+    quoteStorage,
+    addQuote,
+    quoteTotalCount
+)
+
+// Ajout de la citation et de l'auteur dans le localStorage
+function quoteStorage() {
+    const text = inputQuote.value
+    const author = inputAuthor.value
+    const allQuotes = localStorage.getItem('quotes')
+    if (allQuotes === ''){
+        localStorage.setItem('quotes', JSON.stringify([{"author" : author, "text" : text}]))
+    } else {
+        let allQuotesList = JSON.parse(allQuotes)
+        allQuotesList.push({"author" : author, "text" : text})
+        localStorage.setItem('quotes', JSON.stringify(allQuotesList))
+    }
+}
 
 
-btnSubmit.addEventListener('click', addQuote)
+// Affiche les auteurs + citations stockés dans le localStorage
+function addQuote() {
+    const allQuotes = localStorage.getItem('quotes')
+    let allQuotesList = JSON.parse(allQuotes)
 
-// Ajout de la citation et de l'auteur 
-function addQuote(){
-    const quoteP = document.createElement('p')
-    quoteP.className = 'text'
-    quoteP.innerText = document.getElementById('quote-entry').value 
+    allQuotesList.forEach(element => {
+        const quoteP = document.createElement('p')
+        const authorP = document.createElement('p')
+        const divQuote = document.createElement('div')
 
-    const authorP = document.createElement('p')
-    authorP.className = 'author'
-    authorP.innerText = document.getElementById('author-name').value 
+        quoteP.className = 'text'
+        authorP.className = 'author'
+        divQuote.className = 'quote'
 
-    const divQuote = document.createElement('div')
-    divQuote.className = 'quote'
+        divQuote.appendChild(quoteP)
+        divQuote.appendChild(authorP)
+        divQuoteList.appendChild(divQuote)
 
-    divQuote.appendChild(quoteP)
-    divQuote.appendChild(authorP)
-    divQuoteList.appendChild(divQuote)
-
-    quoteStorage()
-
-    quoteCount += 1
-    totalCount = storageCount + quoteCount
-    const count = document.getElementById('count')
-    count.innerText = `${totalCount} citation(s)`
-
+        quoteP.innerText = element.author
+        authorP.innerText = element.text
+    });
 
     inputQuote.value = ''
     inputAuthor.value = ''
+}     
 
-}
+//Permet de compter le nombre de citations enregistrées
+function quoteTotalCount(){
+    const allQuotes = localStorage.getItem('quotes')
+    let allQuotesList = JSON.parse(allQuotes)
 
-//Storage des citations+auteur dans la structure JSON
-function quoteStorage(){
-    text = inputQuote.value
-    author = inputAuthor.value
-
-    newStorage = JSON.parse(localStorage.getItem("text")) || {}
-
-    newStorage[author] = [text]
-
-    localStorage.setItem("text", JSON.stringify(newStorage))
-}
-
-//Affichage des citations+auteurs enregistrés dans le localStorage
-function quoteDisplay(){
-    const newStorage = JSON.parse(localStorage.getItem("text")) || {}
-    for (const author in newStorage) {
-        const quotes = newStorage[author]
-
-        quotes.forEach((quoteText) => {
-
-            const quoteP = document.createElement('p')
-            const authorP = document.createElement('p')
-            const divQuote = document.createElement('div')
-
-            quoteP.className = 'text'
-            authorP.className = 'author'
-            divQuote.className = 'quote'
-
-            quoteP.innerText = quoteText
-            authorP.innerText = author
-
-            divQuote.appendChild(quoteP)
-            divQuote.appendChild(authorP)
-            divQuoteList.appendChild(divQuote)
-    })
-    }
-}
-
-function quoteObjectCount(){
-    storageCount = Object.entries(newStorage).length
-    console.log(storageCount)
-    totalCount = storageCount + quoteCount
-    const count = document.getElementById('count')
+    totalCount = allQuotesList.length + quoteCount
     count.innerText = `${totalCount} citation(s)`
 }
-
-quoteDisplay()
-quoteObjectCount()
-
-
-/*function quoteDelete() {
-    for (const author in localStorage) {
-        localStorage.removeItem(author)
-    }
-}
-*/
-
-//quoteDelete()
-
-/*
- for (author in newStorage) {
-        
-    }
-*/
